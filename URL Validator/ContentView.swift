956d6d551b998eb164149185a8aacc960bdcabe4
Peter Ina <peter.ina@gmail.com>
2019-09-18 08:19:34 -0700

    Initial Commit

---
+++ URL Validator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URL Validator
+//
+//  Created by Peter Ina on 9/18/19.
+//  Copyright © 2019 Peter Ina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
